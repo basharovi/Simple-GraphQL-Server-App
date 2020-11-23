c3de81dc36fe18ebc3217dca73483f99769cea27
Bashar Ovi <bashar.ovi@outlook.com>
2020-11-23 17:01:09 +0600

    Configured DbContext

--- SimpleGraphQLApp/Startup.cs
+++ SimpleGraphQLApp/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleGraphQLApp.Data;
 
 namespace SimpleGraphQLApp
 {
@@ -12,6 +14,7 @@ namespace SimpleGraphQLApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source = GraphQLAppDB"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
