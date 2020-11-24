375a3c3b0e528188772fd1cba850920727207038
Bashar Ovi <bashar.ovi@outlook.com>
2020-11-24 16:37:39 +0600

    Added HotChocolate package & Query

---
+++ SimpleGraphQLApp/Data/Query.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using HotChocolate;
+using HotChocolate.Types;
+using SimpleGraphQLApp.Models;
+
+namespace SimpleGraphQLApp.Data
+{
+    public class Query
+    {
+        [UseSelection]
+        public IQueryable<Student> GetStudents([Service] ApplicationDbContext appContext) =>
+            appContext.Students;
+    }
+}
